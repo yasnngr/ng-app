a6bbb3a0309d7bf4f90f40bce5bf34a8c513292d
Yasin Gungor <yasnngr@gmail.com>
2022-12-12 23:10:11 +0300

    login 2.0

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { AuthResponse } from '../models/auth-response';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -10,6 +12,8 @@ import { AuthService } from '../services/auth.service';
 export class AuthComponent implements OnInit {
 
   isLoginMode:boolean=true;
+  loading=false;
+
   constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
@@ -24,20 +28,23 @@ export class AuthComponent implements OnInit {
     if(!form.valid){//Böylede olur [disable] özelliği ile aynı
       return;
     }
+
+    this.loading=true
     
     const email=form.value.email
     const password=form.value.password
+    let authResponse:Observable<AuthResponse>
 
     if(this.isLoginMode){
-      this.authService.login(email,password).subscribe(response=>{
-        console.log(response)
-      })
+      authResponse=this.authService.login(email,password)
     }else{
-      this.authService.register(email,password).subscribe(response=>{
-        console.log(response)
-      })
+      authResponse=this.authService.register(email,password)
     }
 
+    authResponse.subscribe(response=>{
+      this.loading=false
+      console.log(response)
+    })
   }
 
 }
