1a01da71dc7494eb82c5bc16ba4a602aa745493c
Yasin Gungor <yasnngr@gmail.com>
2022-12-23 18:15:04 +0300

    admin guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { CategoryCreateComponent } from './category-create/category-create.component';
+import { AdminGuard } from './guards/admin-guard';
 import { HomeComponent } from './home/home.component';
 import { ProductCreateComponent } from './product-create/product-create.component';
 import { ProductListComponent } from './product-list/product-list.component';
@@ -9,8 +10,8 @@ import { ProductComponent } from './product-list/product/product.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},//locale host yani ilk sayfa
-  {path:'products/create',component:ProductCreateComponent},
-  {path:'categories/create',component:CategoryCreateComponent},
+  {path:'products/create',component:ProductCreateComponent,canActivate:[AdminGuard]},
+  {path:'categories/create',component:CategoryCreateComponent,canActivate:[AdminGuard]},
   {path:'products',component:ProductListComponent},
   {path:'products/:productId',component:ProductComponent},
   {path:'products/category/:categoryId', component:ProductListComponent},
---
+++ src/app/guards/admin-guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { map, Observable, tap } from "rxjs";
+import { AuthService } from "../services/auth.service";
+
+@Injectable({providedIn:"root"})
+export class AdminGuard implements CanActivate{
+    
+    constructor(
+        private authService:AuthService,
+        private router:Router
+        
+        ){}
+
+    canActivate(
+        route: ActivatedRouteSnapshot, 
+        state: RouterStateSnapshot): boolean | Observable<boolean | UrlTree> | Promise<boolean | UrlTree>{
+        
+            return this.authService.user.pipe(
+                map(user=>{
+                    return !!user && user.email=="yasnngr@gmail.com"
+                }),
+                tap(isAdmin=>{
+                    if(!isAdmin){
+                        this.router.navigate(["/auth"])
+                    }
+                })
+            )
+    }
+
+}
